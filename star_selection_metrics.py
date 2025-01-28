27b19689a3c1a2eb7a57807f253990c288b727ff
Aidan McBride <aidan.mcbride@utah.edu>
2025-01-28 13:51:22 -0700

    evaluating experiments for DIB recreations

--- star_selection_metrics.py
+++ star_selection_metrics.py
@@ -28,7 +28,7 @@ def selectOnEW(tab, vector, select_first = None, norm_vals = None, plot = False)
         plt.show()
 
     if select_first is None:
-        select_first = np.random.choice(len(tab), size = 1)
+        select_first = np.random.choice(len(tab), size = 1)[0]
 
     def next_star(star_idx, vector, direction = +1):
         dist_norm_star = dist_norm[star_idx]
@@ -40,20 +40,20 @@ def selectOnEW(tab, vector, select_first = None, norm_vals = None, plot = False)
         next_star_idx = np.argmin(np.sqrt(diff_dist**2 + diff_ew**2))
         if diff_dist[next_star_idx] == np.inf:
             return np.array([])
-        return next_star_idx
+        return np.array([next_star_idx])
     
     select_above, select_below = True, True
     selection = np.array([select_first])
     while select_above or select_below:
         if select_above:
             next_star_above = next_star(selection[-1], vector, direction = +1)
-            selection = np.concatenate([selection, next_star_above])
+            selection = np.concatenate([selection, next_star_above]).astype(int)
             if len(next_star_above) == 0:
                 select_above = False
         
         if select_below:
             next_star_below = next_star(selection[0], vector, direction = -1)
-            selection = np.concatenate([next_star_below, selection])
+            selection = np.concatenate([next_star_below, selection]).astype(int)
             if len(next_star_below) == 0:
                 select_below = False
 
